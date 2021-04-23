4ab2025a2b28435bb420b0587ce6364f6aa2628f
John Isaiah Sangalang <John Sangalang>
2021-04-23 12:55:18 +1200

    initialised project

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const connectDB = require('./config/db');
+
+const app = express();
+
+
+//connect to DB 
+connectDB();
+
+app.get('/', (req, res) => {
+    res.send("Hello world");
+});
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => {
+    console.log(`Serving running on port ${port}`);
+})

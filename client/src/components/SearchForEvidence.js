334931607d39df27d9973ee0e5b7d328a67bf79d
mht0336 <51040303+tundraarr@users.noreply.github.com>
2021-04-24 15:15:24 +1200

    Updated search page with button and states for handling the options available for the search dropdowns

--- client/src/components/SearchForEvidence.js
+++ client/src/components/SearchForEvidence.js
@@ -1,6 +1,22 @@
 import React, {useState, useEffect} from 'react';
 
 export const SearchForEvidence = () => {
+
+    const [seMethods, setSEMethods] = useState([]);
+    const [claims, setClaims] = useState([]);
+
+    //Temporary SE Methods list
+    const itemsList = ["TDD", "FDD", "SCRUM", "UNIT-TESTING", "AGILE"];
+
+    //Try get the list of SE methods from a database/script list
+    const optionsList = itemsList.map((item) => 
+            <option>{item}</option>
+        );
+        
+    useEffect(() => {
+        console.log("Website loaded!");
+    }, []);
+
     return (
         <div className="container-fluid bg-dark text-light" style={{height: "100vh"}}>
             <div className="row">
@@ -21,7 +37,8 @@ export const SearchForEvidence = () => {
                             <label className="lead fs-2">SE Practice:</label>
                             <select className="form-control" name="se-practice">
                                 <option disabled hidden selected>Select SE Practice</option>
-                                <option value="TDD">TDD</option>
+                                {/* <option value="TDD">TDD</option> */}
+                                {optionsList}
                             </select>
                             <br></br>
 
@@ -35,6 +52,8 @@ export const SearchForEvidence = () => {
                             <label className="lead fs-2">Year Range:</label>
                             <input className="form-control" type="text" name="year-start" placeholder="Start Year"></input>
                             <input className="form-control" type="text" name="year-end" placeholder="End Year"></input>
+
+                            <button className="btn btn-default bg-dark text-light border border-light mt-3" type="submit">Search</button>
                         </div>
                     </form>
                 </div>

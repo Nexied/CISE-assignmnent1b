5dbe3350db4cf9251166dc67b410953b3a14763b
mht0336 <51040303+tundraarr@users.noreply.github.com>
2021-04-26 12:10:26 +1200

    Added a filtered searchable dropdown to the search page - will require installation of react-select

--- client/src/components/EvidenceSearch.js
+++ client/src/components/EvidenceSearch.js
@@ -1,17 +1,31 @@
 import React, {useState, useEffect} from 'react';
+import Select from 'react-select';
 
-export const SearchForEvidence = () => {
+export const EvidenceSearch = () => {
 
     const [seMethods, setSEMethods] = useState([]);
     const [claims, setClaims] = useState([]);
 
     //Temporary SE Methods list
-    const itemsList = ["TDD", "FDD", "SCRUM", "UNIT-TESTING", "AGILE"];
+    const seList = [
+        { label: "TDD", value: "TDD"},
+        { label: "FDD", value: "FDD"},
+        { label: "SCRUM", value: "SCRUM"},
+        { label: "UNIT-TESTING", value: "UNIT-TESTING"},
+        { label: "AGILE", value: "AGILE"},
+    ];
+
+    const claimsList = [
+        { label: "Improves Code Quality", value: "Improves Code Quality"},
+        { label: "More efficient code production", value: "Code Production Efficiency"},
+    ];
+
+
 
     //Try get the list of SE methods from a database/script list
-    const optionsList = itemsList.map((item) => 
-            <option>{item}</option>
-        );
+    // const optionsList = itemsList.map((item) => 
+    //         <option>{item}</option>
+    //     );
         
     useEffect(() => {
         console.log("Website loaded!");
@@ -35,23 +49,28 @@ export const SearchForEvidence = () => {
                         <div className="form-group col-3">
 
                             <label className="lead fs-2">SE Practice:</label>
-                            <select className="form-control" name="se-practice">
+                            {/* <select className="form-control" name="se-practice">
                                 <option disabled hidden selected>Select SE Practice</option>
-                                {/* <option value="TDD">TDD</option> */}
                                 {optionsList}
-                            </select>
+                            </select> */}
+                            <Select className="text-dark"
+                                options={seList} 
+                            />
                             <br></br>
 
                             <label className="lead fs-2">Claim:</label>
-                            <select className="form-control" name="claim">
+                            {/* <select className="form-control" name="claim">
                                 <option disabled hidden selected>Select Claim</option>
                                 <option value="Improves Code Quality">Improves Code Quality</option>
-                            </select>
+                            </select> */}
+                            <Select className="text-dark"
+                                options={claimsList} 
+                            />
                             <br></br>
 
                             <label className="lead fs-2">Year Range:</label>
-                            <input className="form-control" type="text" name="year-start" placeholder="Start Year"></input>
-                            <input className="form-control" type="text" name="year-end" placeholder="End Year"></input>
+                            <input className="form-control" type="text" name="year-start" maxLength="4" placeholder="Start Year e.g. 2000"></input>
+                            <input className="form-control" type="text" name="year-end" maxLength="4" placeholder="End Year e.g. 2010"></input>
 
                             <button className="btn btn-default bg-dark text-light border border-light mt-3" type="submit">Search</button>
                         </div>

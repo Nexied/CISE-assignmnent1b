e91a2449a9b9f2f2b7a4226547535c6f4115f171
tundraarr <51040303+tundraarr@users.noreply.github.com>
2021-04-27 13:22:51 +1200

    Evidence cards (#3)

---
+++ client/src/components/EvidenceCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const EvidenceCard = ({evidenceData}) => {
+
+    const evidence = evidenceData;
+    console.log(evidence.article);
+
+    return (
+        <div className="container-fluid bg-dark text-light border border-light">
+            <h2>{evidence.article}</h2>
+            <p>{evidence.author}</p>
+            <p>{evidence.title}</p>
+        </div>
+    )
+}
